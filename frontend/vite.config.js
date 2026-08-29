22aa3a671baa2705e2d7fb670411d1e736d69972
Dreamlone <mik_sar@mail.ru>
2026-08-30 00:09:10 +0300

    Add Quarto & rename one plot file name

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/linear-regression/",
+  base: "/linear-regression/interactive/",
   plugins: [react()],
 });
